841946cd1d2a6675b2a48b2d94a2075b31593b46
Taras Yarovets <taras.yarovets@gmail.com>
2023-10-05 03:17:46 +0300

    Added second task of the first lection.

---
+++ Lection 1/Task 2/main.py
@@ -0,0 +1,5 @@
+print ("Enter 2 numbers to add them for export one number")
+num_1 = int(input("Set first number "))
+num_2 = int(input("Set second number "))
+sum = num_1 + num_2
+print ("Your answer - ", sum )
