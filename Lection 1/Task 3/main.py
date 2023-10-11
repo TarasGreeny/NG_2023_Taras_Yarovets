80abbbadd83ef0d39809d9acc82002387115e2ea
Taras Yarovets <taras.yarovets@gmail.com>
2023-10-12 00:30:24 +0300

    Added third task of the first lection.

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,4 +1,9 @@
-print ("Write your °C and this calculator conversion to Fahrenheit")
-a = int(input("Set your °C- "))
-sum = (a * 9/5) + 32
-print ("Your Fahrenheit:", sum)
+print ("Please, write your temperature and choose what do you want to translate this into? (°C or °F)")
+b = int(input("Write your temperature: "))
+a = int(input("Write what your temperature is measured in C(write 0) or F(write 1): "))
+if a==0:
+    sum = (b*9/5)+32
+    print ("Here is your result in Celsius: ", sum )
+if a==1:
+    sum = (b-32)*5/9
+    print ("Here is your result in Fahrenheit: ", sum )
