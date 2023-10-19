dabe6a27b5126424dacbadc9865e2dc1ac22f0bb
Taras Yarovets <taras.yarovets@gmail.com>
2023-10-19 21:47:49 +0300

    Added third task of the first lection.

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,9 +1,9 @@
 print ("Please, write your temperature and choose what do you want to translate this into? (°C or °F)")
-b = int(input("Write your temperature: "))
-a = int(input("Write what your temperature is measured in C(write 0) or F(write 1): "))
-if a==0:
-    sum = (b*9/5)+32
+temper = int(input("Write your temperature: "))
+degree = int(input("Write what your temperature is measured in C(write 0) or F(write 1): "))
+if temper==0:
+    sum = (degree*9/5)+32
     print ("Here is your result in Celsius: ", sum )
-if a==1:
-    sum = (b-32)*5/9
+if temper==1:
+    sum = (degree-32)*5/9
     print ("Here is your result in Fahrenheit: ", sum )
